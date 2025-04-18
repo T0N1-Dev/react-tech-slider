52199e14f04bc5253659a2accaa0c00e3c0e21aa
Antonio Cruz <antoniocruzglez24@gmail.com>
2025-04-18 12:42:31 -0400

    test(slider): add unit tests and release new version

---
+++ dist/src/Slider.d.ts
@@ -0,0 +1,15 @@
+import './slider.css';
+export interface Brand {
+    id: number;
+    name: string;
+    img: string;
+}
+export interface SliderProps {
+    brandsList: Brand[];
+    borderWidth?: number;
+    borderColor?: string;
+    backgroundColor?: string;
+    iconWidth?: number;
+    iconHeight?: number;
+}
+export declare const Slider: React.FC<SliderProps>;
